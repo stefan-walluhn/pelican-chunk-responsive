82a116db047db18293ae85f34ef524af8f5ad8ad
Stefan@Terminal.21 <stefan@terminal21.de>
2015-02-23 21:54:34 +0100

    add chunk css

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,32 +5,59 @@ module.exports = function(grunt) {
         bower: {
             install: {
                 options: {
-                    targetDir: 'static',
-                    cleanBowerDir: true
+                    targetDir: './static'
                 }
             }
         },
-        subgrunt: {
-            bootstrap: {
-                'bower_components/bootstrap/': 'dist'
+        less: {
+            chunk: {
+                files: {
+                    'static/chunk/theme.css': 'src/less/theme.less'
+                }
             }
         },
-        wiredep: {
-            task: {
-                src: [
-                    'src/index.html'
-                ],
-                options: {
-                    directory: 'lib'
+        uglify: {
+            jquery: {
+                files: {
+                    'static/jquery/jquery.min.js': ['static/jquery/jquery.js']
+                }
+            },
+            bootstrap: {
+                files: {
+                    'static/bootstrap/bootstrap.min.js': ['static/bootstrap/bootstrap.js']
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                compatibility: 'ie8',
+                advanced: false
+            },
+            bootstrap: {
+                files: {
+                    'static/bootstrap/bootstrap.min.css': ['static/bootstrap/bootstrap.css']
+                }
+            },
+            chunk: {
+                files: {
+                    'static/chunk/theme.min.css': ['static/chunk/theme.css']
                 }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-bower-task');
-    grunt.loadNpmTasks('grunt-subgrunt');
-    grunt.loadNpmTasks('grunt-wiredep');
-
-    grunt.registerTask('default', ['bower:install'])
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask(
+        'default',
+        [
+            'bower:install',
+            'less:chunk',
+            'uglify:jquery',
+            'uglify:bootstrap',
+            'cssmin:bootstrap',
+            'cssmin:chunk'])
 };
 
